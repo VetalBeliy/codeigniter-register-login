8ce7299b42966e4ec5dbfe37aab713ac98f5c4fc
VetalBeliy <vbelokurenko@gmail.com>
2016-02-12 15:31:17 +0200

    controller, model, view

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://codeigniter_register-login/';
+$config['base_url'] = 'http://codeigniter-register-login/';
 
 /*
 |--------------------------------------------------------------------------
